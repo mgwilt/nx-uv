cf238f8aa824a551c27285181cd62d4285638695
mike <571232+mgwilt@users.noreply.github.com>
2026-03-01 11:08:34 -0800

    chore(repo): bootstrap standalone nx plugin workspace

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import nx from '@nx/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.json'],
+    rules: {},
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser')
+    }
+  },
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist', '**/out-tsc', '**/vitest.config.*.timestamp*']
+  },
+  {
+    files: [
+      '**/package.json',
+      '**/executors.json',
+      '**/generators.json'
+    ],
+    rules: {
+      '@nx/nx-plugin-checks': 'error'
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser')
+    }
+  },
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+            '{projectRoot}/vite.config.{js,ts,mjs,mts}'
+          ]
+        }
+      ]
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser')
+    }
+  }
+];
